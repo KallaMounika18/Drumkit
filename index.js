cd29a028b29c4cb3820861ac0fec681eb9d380a6
Kalla Mohana Mounika <77119113+KallaMounika18@users.noreply.github.com>
2023-07-08 22:05:26 +0530

    index.js

--- index.js
+++ index.js
@@ -19,37 +19,37 @@ document.addEventListener("keypress", function (event) {
 function makeSound(key) {
   switch (key) {
     case "d":
-      var audio = new Audio("sounds/crash.mp3");
+      var audio = new Audio("crash.mp3");
       audio.play();
       break;
 
     case "r":
-      var audio = new Audio("sounds/kick-bass.mp3");
+      var audio = new Audio("kick-bass.mp3");
       audio.play();
       break;
 
     case "u":
-      var audio = new Audio("sounds/snare.mp3");
+      var audio = new Audio("snare.mp3");
       audio.play();
       break;
 
     case "m":
-      var audio = new Audio("sounds/tom-1.mp3");
+      var audio = new Audio("tom-1.mp3");
       audio.play();
       break;
 
     case "k":
-      var audio = new Audio("sounds/tom-2.mp3");
+      var audio = new Audio("tom-2.mp3");
       audio.play();
       break;
 
     case "i":
-      var audio = new Audio("sounds/tom-3.mp3");
+      var audio = new Audio("tom-3.mp3");
       audio.play();
       break;
 
     case "t":
-      var audio = new Audio("sound/tom-4.mp3");
+      var audio = new Audio("tom-4.mp3");
       audio.play();
       break;
 
